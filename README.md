b4391c5efbb14e361958d901451b02342107a592
Szymon Wygnanski <s@finalclass.net>
2015-03-03 12:07:05 +0100

    first release

--- README.md
+++ README.md
@@ -3,3 +3,15 @@ Ajax Uploader
 
 Ajax file uploader (ES6)
 
+function ajaxUpload({
+  files = [],
+  url = '',
+  data = {},
+  method = 'POST',
+  onError = ()=>undefined,
+  onProgress = ()=>undefined,
+  onSuccess = ()=>undefined,
+  filesFieldName = 'files'
+  }) {
+  ...
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,41 @@
-export class Test {
-  go() {
-    return 7;
+function ajaxUpload({
+  files = [],
+  url = '',
+  data = {},
+  method = 'POST',
+  onError = ()=>undefined,
+  onProgress = ()=>undefined,
+  onSuccess = ()=>undefined,
+  filesFieldName = 'files'
+  }) {
+
+  files = Array.isArray(files) ? files : [files];
+
+  var xhr = new XMLHttpRequest();
+  var formData = new FormData();
+
+  files.forEach(function (file) {
+    formData.append(filesFieldName + '[]', file, file.name);
+  });
+  Object.keys(data || {}).forEach(function (key) {
+    formData.append(key, data[key])
+  });
+
+  xhr.open(method, url, true);
+  if (xhr.upload) {
+    xhr.upload.onprogress = onProgress;
   }
+
+  xhr.onload = function () {
+    ajaxing(-1);
+    if (xhr.status >= 200 && xhr.status <= 400) {
+      onSuccess(xhr.responseBody || JSON.parse(xhr.responseText), xhr);
+    } else {
+      onError(new Error(xhr.responseBody));
+    }
+  };
+
+  xhr.send(formData);
 }
+
+export {ajaxUpload};
